e9003d44b5187fb2ba0218dc8fc95d9d0d73f603
Enamour <debeila.mae@gmail.com>
2022-12-09 19:33:06 -0800

    3 corr

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char letter;
 
-	for  (letter = 'a'; letter <= 'z'; letter++
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchat(letter);
 
 	putchar('\n');
-        return (0);
+	return (0);
 }
