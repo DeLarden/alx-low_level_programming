4d7e3b3f009b92c21d7e5816b9cbf78c290d3e4c
DeLarden <debeila.mae@gmail.com>
2023-01-06 18:39:43 -0800

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string in reverse
+ * @s: string
+ * Retrun: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	char s[] = "Puts with recursion";
+
+	return (void);
+}
