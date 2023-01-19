65eadb60516b970d39d5bceeeb6d8f8dcb770f47
DeLarden <debeila.mae@gmail.com>
2023-01-19 12:26:11 -0800

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (separator == NULL)
 		separator = "";
 
-	for (i = 0; i< n; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ap, char*);
 		if (str == NULL)
